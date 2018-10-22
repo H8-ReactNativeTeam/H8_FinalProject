fa71fee172eae7985b04a7d8ffc1be5d808f0650
knighttrader <knighttrader@gmail.com>
2018-10-22 14:39:16 +0700

    Update RegisterEmail

---
+++ src/screens/RegisterName/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Actions } from "react-native-router-flux";
+import {
+  Icon,
+  FormLabel,
+  FormInput,
+  FormValidationMessage
+} from "react-native-elements";
+import styles from "./style";
+
+class RegisterName extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleChangeFirst(text) {
+    this.setState({
+      firstName: text
+    });
+  }
+
+  handleChangeLast(text) {
+    this.setState({
+      lastName: text
+    });
+  }
+
+  handleLoginPress() {
+    Actions.home();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Icon name="arrow-back" iconStyle={styles.iconBack} />
+        <Text style={styles.title}>What's your name?</Text>
+        <FormLabel labelStyle={styles.label}>FIRST NAME</FormLabel>
+        <FormInput inputStyle={styles.textInput} onChangeText={null} />
+        <FormLabel labelStyle={styles.label}>LAST NAME</FormLabel>
+        <FormInput
+          onChangeText={e => this.handleChangeLast(e)}
+          inputStyle={styles.textInput}
+        />
+        <Icon
+          name="chevron-with-circle-right"
+          type="entypo"
+          iconStyle={styles.iconNext}
+          onPress={Actions.registerEmail}
+        />
+      </View>
+    );
+  }
+}
+
+export default RegisterName;
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,7 @@
 import Welcome from "./Welcome";
 import Login from "./Login";
-import Register from "./Register";
+import RegisterName from "./RegisterName";
+import RegisterEmail from "./RegisterEmail";
 import Home from "./Home";
 
-export { Welcome, Login, Register, Home };
+export { Welcome, Login, RegisterName, RegisterEmail, Home };
